2039e9d80c4717add0d6ce85d284206d273f1d05
Kilian Tyler <kilian@ktyler.me>
2022-12-18 11:46:36 -0500

    update

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+sed -i "s/.*${K8S_IP}.*//" ~/.ssh/known_hosts
 k3sup install --k3s-version  v1.24.8+k3s1 --ip ${K8S_IP} --user root --k3s-extra-args '--disable traefik' --local-path $HOME/.kube/config-files/kflix.yaml --context kflix --ssh-key $HOME/.ssh/id_ed25519
 kubectx kflix
 export GITHUB_PASSWORD="op://2fa/github/Argo"
@@ -7,16 +8,6 @@ export AKEYLESS_ACCESSID="op://Personal/Akeyless/accessID"
 export AKEYLESS_ACCESSTYPEPARAM="op://Personal/Akeyless/AccessTypeParam"
 op run --no-masking -- envsubst < startup-secrets.yaml | kubectl apply -f -
 helm dependency build helm-charts/argo-cd && helm upgrade --install -n argo-cd -f helm-charts/argo-cd/values.yaml argo-cd helm-charts/argo-cd
-# helm upgrade --install -n argo-cd -f helm-charts/cluster-bootstrap/values.yaml argo-bootstrap helm-charts/cluster-bootstrap
-
-
-
-
-
-
-
-
-
 
 
 
