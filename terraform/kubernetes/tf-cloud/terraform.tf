1fa5463b8c5208d8155f735e23da9b94616b83e4
Kilian Tyler <github@ktyler.me>
2023-10-05 19:51:02 -0400

    feat(terraform): add S3 Backend for tf-cloud terraform

--- terraform/kubernetes/tf-cloud/terraform.tf
+++ terraform/kubernetes/tf-cloud/terraform.tf
@@ -5,4 +5,14 @@ terraform {
       version = "~> 0.49.0"
     }
   }
+  backend "s3" {
+    bucket = var.BUCKET_NAME
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+    endpoints {
+      s3 = var.BUCKET_HOST
+    }
+  }
 }
--- terraform/kubernetes/tf-cloud/variables.tf
+++ terraform/kubernetes/tf-cloud/variables.tf
@@ -10,3 +10,21 @@ variable "tfe_organization" {
 variable "tfe_project" {
   type = string
 }
+
+variable "BUCKET_NAME" {
+  type = string
+}
+
+variable "BUCKET_HOST" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
