b779825cca21aec6282901f21e95573405b168af
Kilian Tyler <github@ktyler.me>
2024-03-03 16:00:38 -0500

    chore: linting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,17 @@
-variable "CLOUDFLARE_API_TOKEN" {}
+variable "CLOUDFLARE_API_TOKEN" {
+  description = "Cloudflare API Token"
+  type        = string
+}
 
-variable "OVH_APPLICATION_KEY" {}
-variable "OVH_APPLICATION_SECRET" {}
-variable "OVH_CONSUMER_KEY" {}
+variable "OVH_APPLICATION_KEY" {
+  description = "OVH Application Key"
+  type        = string
+}
+variable "OVH_APPLICATION_SECRET" {
+  description = "OVH Application Secret"
+  type        = string
+}
+variable "OVH_CONSUMER_KEY" {
+  description = "OVH Consumer Key"
+  type        = string
+}
