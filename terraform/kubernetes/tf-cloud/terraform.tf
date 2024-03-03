f546fb8611886916c824b9e93148906cfa42c942
Kilian Tyler <github@ktyler.me>
2024-03-03 15:55:13 -0500

    chore: linting

--- terraform/kubernetes/tf-cloud/terraform.tf
+++ terraform/kubernetes/tf-cloud/terraform.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = var.BUCKET_NAME
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket     = var.BUCKET_NAME
+    key        = "terraform.tfstate"
+    region     = "us-east-1"
     access_key = var.AWS_ACCESS_KEY_ID
     secret_key = var.AWS_SECRET_ACCESS_KEY
     endpoints {
